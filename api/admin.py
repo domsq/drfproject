1628a5ad731ca9525205e035dd107f00fa8a54ef
Dominic Quail <Dominic.Quail@oraclefinance.co.uk>
2026-03-10 17:37:55 +0000

    Configure serializer and views

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Task
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'completed')
+    list_filter = ('completed',)
+    search_fields = ('title',)
+
+
