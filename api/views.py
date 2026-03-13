e656323c44fc2945fb665e878036afb72a09b286
Dominic Quail <Dominic.Quail@oraclefinance.co.uk>
2026-03-13 16:34:54 +0000

    Add string method and basic permissions

--- api/views.py
+++ api/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework import permissions
 from .models import Task
 from .serializers import TaskSerializer
 
 
 class TaskViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticated]
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
 
